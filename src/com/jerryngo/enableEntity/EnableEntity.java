c38ae4c061a8388770925a48175c2fb4ae3fe9f0
Jerry2001 <ngohongphuc2001@gmail.com>
2020-11-15 06:57:44 -0600

    First working version, make mob aware every 2s for 50s

--- src/com/jerryngo/enableEntity/EnableEntity.java
+++ src/com/jerryngo/enableEntity/EnableEntity.java
@@ -1,14 +1,36 @@
 package com.jerryngo.enableEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
+import org.bukkit.entity.Entity;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import net.minecraft.server.v1_16_R3.*;
 
 public class EnableEntity extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		
+		BukkitScheduler scheduler = getServer().getScheduler();
+		scheduler.scheduleSyncRepeatingTask(this, (Runnable)()->{
+			int i = 0;
+			World w = Bukkit.getWorld("world");
+			for(Entity e : w.getEntities()) {
+				net.minecraft.server.v1_16_R3.Entity nms = ((CraftEntity)e).getHandle();
+				if(nms instanceof EntityInsentient) {
+					if(!((EntityInsentient)nms).aware) {
+						((EntityInsentient)nms).aware = true;
+					}
+					getLogger().info("Fix " + i + "th Frozen Mob");
+					i += 1;
+				}
+			}
+		}, 40, 1000);
 	}
 	
 	@Override
 	public void onDisable() {
 		
 	}
+	
 }
