eff979c3000ad04cdc916ceabbfc7e8f3b1b6bb2
Jerry2001 <ngohongphuc2001@gmail.com>
2020-11-15 05:25:59 -0600

    First compileable blank plugin

---
+++ src/com/jerryngo/enableEntity/EnableEntity.java
@@ -0,0 +1,14 @@
+package com.jerryngo.enableEntity;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnableEntity extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
